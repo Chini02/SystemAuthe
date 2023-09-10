047486ea7a47ce48e48928a31e0f661993a46481
hamza <hamzachini00@gmail.com>
2023-09-10 14:41:55 -0700

    Started Validation Login

--- login.php
+++ login.php
@@ -1,5 +1,18 @@
 <?php
+require "congif/config.php";
+if($_POST["submit"]){
+    $email   = $_POST["email"];
+    $pasword = $_POST["pasword"];
+    if($email == "" || $password == ""){
+        echo "All fiels are required";
+    }
+    $hasch_password = password_hash($pasword,PASSWORD_DEFAULT);
+    $sql = "SELECT email , password FROM users WHERE email = '$email' ";
+    $check = $conn->prepare($sql);
+    $check->execute([
 
+    ]);
+}
 
 ?>
 
@@ -12,18 +25,18 @@
             <form action="" methode="POST">
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
+                    <input type="password" name="pasword" class="form-control" id="exampleInputPassword1">
                 </div>
                 <div class="mb-3">
                     <a href="register.php">
                         Register
                     </a>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button name="submit" type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
     </div>
--- register.php
+++ register.php
@@ -1,8 +1,33 @@
 <?php
+require "config/config.php"; ?>
 
+<?php
+if(isset($_POST["submit"])){
+    $name = $_POST["fname"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $rpassword = $_POST["rpassword"];
+    if($name == "" || $email == "" || $password == "" || $rpassword == ""){
+        echo "All Fiels are required" ;
+    }
+    elseif ($password != $rpassword) {
+        echo "Passwords do not match";
+    }
+    else {
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
+        $insert = $conn->prepare($sql);
+        $insert->execute(
+            [
+                ":name"     => $name,
+                ":email"    => $email,
+                ":password" => $hashed_password,
+            ]
+        );
 
+    }
+}
 ?>
-
 
 <?php include "header.php"; ?>
 <!--  -->
@@ -10,29 +35,30 @@
 <div class="container">
     <div class="container-fluid m-4">
             <div class="container">
-            <form action="" methode="POST">
+            <form action="" method="post">
                 <div class="mb-3 row">
-                    <label for="inputPassword" class="col-sm-2 col-form-label">Name</label>
+                    <label for="inputPassword" class="col-sm-2 col-form-label">UserName :</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputPassword">
+                        <input type="text" name="fname" class="form-control" id="inputPassword" placeholder="Full Name ...">
+                        
                     </div>
                 </div>
                 <div class="mb-3 row">
-                    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+                    <label for="staticEmail" class="col-sm-2 col-form-label">Email :</label>
                     <div class="col-sm-10">
-                        <input type="email" class="form-control" id="staticEmail" value="">
+                        <input type="email" name="email" class="form-control" id="staticEmail" placeholder="Email ...">
                     </div>
                 </div>
                 <div class="mb-3 row">
                     <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
                     <div class="col-sm-10">
-                        <input type="password" class="form-control" id="inputPassword">
+                        <input type="password" name="password" class="form-control" id="inputPassword" placeholder="Password ...">
                     </div>
                 </div>
                 <div class="mb-3 row">
-                    <label for="inputPassword" class="col-sm-2 col-form-label">Repeat Password</label>
+                    <label for="inputPassword" name="" class="col-sm-2 col-form-label">Repeat Password</label>
                     <div class="col-sm-10">
-                        <input type="password" class="form-control" id="inputPassword">
+                        <input type="password" name="rpassword" class="form-control" id="inputPassword" placeholder="Password ...">
                     </div>
                 </div>
                 <div class="mb-3">
@@ -40,7 +66,7 @@
                         Already you have account?
                     </a>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" name="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
     </div>
